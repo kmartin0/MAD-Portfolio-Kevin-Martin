8cb96455eb6c24a2b5e0a01f06f1c7cf928aefb7
Kevin Martin <kevin.martin@live.nl>
2018-09-06 13:00:21 +0200

    Initial Geo Guess Swipe project for assignment 2

---
+++ 02GeoGuessSwipe/app/src/main/java/com/kevin/a02geoguessswipe/ui/GeoGuessSwipeActivity.kt
@@ -0,0 +1,14 @@
+package com.kevin.a02geoguessswipe.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.kevin.a02geoguessswipe.R
+
+class GeoGuessSwipeActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_geo_guess_swipe_activity)
+	}
+
+}
