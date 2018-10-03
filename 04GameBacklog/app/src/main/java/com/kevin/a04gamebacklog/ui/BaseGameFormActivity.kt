3d820b40e4cc38d9899a0d443c3c712f2c6ba739
Kevin Martin <kevin.martin@hva.nl>
2018-10-03 14:19:45 +0200

    Add toolbar back button assignment 4

--- 04GameBacklog/app/src/main/java/com/kevin/a04gamebacklog/ui/BaseGameFormActivity.kt
+++ 04GameBacklog/app/src/main/java/com/kevin/a04gamebacklog/ui/BaseGameFormActivity.kt
@@ -2,6 +2,7 @@ package com.kevin.a04gamebacklog.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import android.widget.ArrayAdapter
 import com.kevin.a04gamebacklog.R
 import com.kevin.a04gamebacklog.database.GameRepository
@@ -14,59 +15,72 @@ import java.util.*
 
 abstract class BaseGameFormActivity : AppCompatActivity() {
 
-	protected lateinit var spinnerAdapter: ArrayAdapter<String>
-	protected var bundle: Bundle? = null
-	protected lateinit var gameRepository: GameRepository
+    protected lateinit var spinnerAdapter: ArrayAdapter<String>
+    protected var bundle: Bundle? = null
+    protected lateinit var gameRepository: GameRepository
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_add_game)
-		gameRepository = GameRepository(this)
-		bundle = intent.extras
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_game)
+        title = "Game Backlog Manager"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-		spinnerAdapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, statusArr)
-		fab_save_game.setOnClickListener { onSaveClick() }
-		spin_status.adapter = spinnerAdapter
-	}
+        gameRepository = GameRepository(this)
+        bundle = intent.extras
 
-	private fun onSaveClick() {
-		if (isFormFilledIn()) {
-			val game = Game(et_title.text.toString(),
-					et_platform.text.toString(),
-					et_notes.text.toString(),
-					spin_status.selectedItem as String,
-					SimpleDateFormat("dd/MM/yyy", Locale("nl-NL")).format(Date()))
+        spinnerAdapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, statusArr)
+        fab_save_game.setOnClickListener { onSaveClick() }
+        spin_status.adapter = spinnerAdapter
+    }
 
-			if (bundle?.getSerializable(GAME_EXTRA) != null) game.gameId = (bundle?.getSerializable(GAME_EXTRA) as Game).gameId
+    private fun onSaveClick() {
+        if (isFormFilledIn()) {
+            val game = Game(et_title.text.toString(),
+                    et_platform.text.toString(),
+                    et_notes.text.toString(),
+                    spin_status.selectedItem as String,
+                    SimpleDateFormat("dd/MM/yyy", Locale("nl-NL")).format(Date()))
 
-			onSave(game)
-		}
-	}
+            if (bundle?.getSerializable(GAME_EXTRA) != null) game.gameId = (bundle?.getSerializable(GAME_EXTRA) as Game).gameId
 
-	private fun isFormFilledIn(): Boolean {
-		return when {
-			et_title.text.isNullOrBlank() -> {
-				showSnackbar("Please fill in a title", cl_add_game); false
-			}
-			et_platform.text.isNullOrBlank() -> {
-				showSnackbar("Please fill in a platform", cl_add_game); false
-			}
-			et_notes.text.isNullOrBlank() -> {
-				showSnackbar("Please fill in your notes", cl_add_game); false
-			}
-			else -> true
-		}
-	}
+            onSave(game)
+        }
+    }
 
-	abstract fun onSave(game: Game)
+    private fun isFormFilledIn(): Boolean {
+        return when {
+            et_title.text.isNullOrBlank() -> {
+                showSnackbar("Please fill in a title", cl_add_game); false
+            }
+            et_platform.text.isNullOrBlank() -> {
+                showSnackbar("Please fill in a platform", cl_add_game); false
+            }
+            et_notes.text.isNullOrBlank() -> {
+                showSnackbar("Please fill in your notes", cl_add_game); false
+            }
+            else -> true
+        }
+    }
 
-	companion object {
-		val statusArr = arrayOf(
-				"Want to Play",
-				"Playing",
-				"Stalled",
-				"Dropped"
-		)
-	}
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    abstract fun onSave(game: Game)
+
+    companion object {
+        val statusArr = arrayOf(
+                "Want to Play",
+                "Playing",
+                "Stalled",
+                "Dropped"
+        )
+    }
 
 }
