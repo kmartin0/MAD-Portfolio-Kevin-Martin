83e2a34fd0e29f471ced5657f669a674d78382c4
Kevin Martin <kevin.martin@live.nl>
2018-11-15 16:59:02 +0100

    Implemented layoutdirection for assignment 5 numbers list.

--- 05NumberTrivia/app/src/main/java/com/kevin/a05numbertrivia/ui/NumbersAdapter.kt
+++ 05NumberTrivia/app/src/main/java/com/kevin/a05numbertrivia/ui/NumbersAdapter.kt
@@ -2,6 +2,7 @@ package com.kevin.a05numbertrivia.ui
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.util.LayoutDirection
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,13 +15,17 @@ class NumbersAdapter : RecyclerView.Adapter<NumbersAdapter.ViewHolder>() {
     private var numbers: List<Number>? = null
     private lateinit var context: Context
 
+    /**
+     * Creates the viewholder, sets layout direction to ltr and rtl for viewtype zero and one respectively.
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         this.context = parent.context
-        val v = when (viewType) {
-            0 -> LayoutInflater.from(context).inflate(R.layout.item_number, parent, false)
-            1 -> LayoutInflater.from(context).inflate(R.layout.item_number_reverse, parent, false)
-            else -> LayoutInflater.from(context).inflate(R.layout.item_number, parent, false)
-        }
+		val v = LayoutInflater.from(context).inflate(R.layout.item_number, parent, false)
+
+		when (viewType){
+			0 -> v.layoutDirection = View.LAYOUT_DIRECTION_LTR
+			1 -> v.layoutDirection = View.LAYOUT_DIRECTION_RTL
+		}
 
         return ViewHolder(v)
     }
@@ -36,6 +41,9 @@ class NumbersAdapter : RecyclerView.Adapter<NumbersAdapter.ViewHolder>() {
         }
     }
 
+    /**
+     * Return zero for even and one for even position.
+     */
     override fun getItemViewType(position: Int): Int {
         return when {
             position % 2 == 0 -> 0
